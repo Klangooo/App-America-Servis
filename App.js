a5ed94b2f7201c0bfb90885e1a135f6f460d798b
Larissa <larissaquiri@hotmail.com>
2020-11-06 17:15:41 -0300

    formulario

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
+import logoImg from './assets/icon.png';
 
 export default function App() {
+//
+    state = {
+    cpf: '',
+    }
+
+  render(); {
+    const UserList = ({ cpf }) => {
+      if (cpf == "123.456.789-10") {
+          return 1;}
+      else {
+        return 0;}
+      }
+    }
+//
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image 
+       source={logoImg}
+       style={styles.logo}
+      />
+
+     <Text
+      style={styles.title}
+      >Bem-vindo!</Text>
       <StatusBar style="auto" />
+
+//
+    <TextInput
+      secureTextEntry = {true}
+      style={styles.input}
+      onChangeText = { text => this.state.cpf = text}
+      placeholder="Digite seu CPF:" />
+
+//
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
+
+  title: {
+    fontSize: 30,
+    marginBottom: 300,
+    marginTop: 150,
+    color: '#032066',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  },
+  
+  logo: {
+    width: 80,
+    height: 80,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E5E6E8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input:{
+    marginTop: 10,
+    padding: 10,
+    width:300,
+    backgroundColor:'#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderRadius: 3,
+},
 });
