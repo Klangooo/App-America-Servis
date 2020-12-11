a68bbcd224b70e5b2e4a93cb7d16241df1b3f500
Ana Carolina Miziara <aninhamiziara@gmail.com>
2020-12-11 01:44:23 -0300

    Tela 1 e routes

---
+++ src/pages/Tela 1/index.js
@@ -0,0 +1,96 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component, useState } from 'react';
+import { StyleSheet, TouchableOpacity, Alert, Text, View, Image, TextInput, Keyboard } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+import logoImg from './assets/icon.png';
+
+  export default class Inicio extends Component {
+
+  render() {
+  return (
+
+    <View style={styles.container}>
+      <Image 
+       source={logoImg}
+       style={styles.logo}
+      />
+
+      <Text
+        style={styles.title}>Bem-vindo!
+      </Text>
+      <StatusBar style="auto" />
+
+      <TouchableOpacity 
+          style = {styles.button}
+          onPress = {() => navigateToIndividual(tela1) }>
+        <Text style = {styles.buttonText}>Individual</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+          style = {styles.button}
+          onPress = {() => navigateToColetivo(tela1) }>
+        <Text style = {styles.buttonText}>Coletivo</Text>
+      </TouchableOpacity>
+
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  button: {
+    borderRadius: 5,
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    backgroundColor: '#0e72Be',
+    shadowColor: 'black',
+    shadowOpacity: 0.4,
+    shadowRadius: 5,
+    shadowOffset: {
+    height: 1,
+    width: 1 },
+    marginTop: 20
+  },
+
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 15,
+    textAlign: 'center'
+  },
+
+  title: {
+    fontSize: 25,
+    marginBottom: 100,
+    marginTop: 0,
+    color: '#032066',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  },
+
+  sub: {
+    fontSize: 18,
+    color: '#0e72Be',
+    fontWeight: 'bold',
+  },
+  
+  logo: {
+    width: 80,
+    height: 80,
+    marginTop: -80,
+    marginBottom: 10 
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: '#E5E6E8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+ 
+});
---
+++ src/services/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Individual from './pages/Individual';
+import Coletivo from './pages/Coletivo';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Individual" component={Individual} />
+                <AppStack.Screen name="Coletivo" component={Coletivo} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
